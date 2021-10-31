a0d579b3181e4a849e323ef0f1beb3b00c431b07
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-10-31 14:00:18 +0200

    ggg
    gg

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import Header from "./components/Header/Header";
 import Menu from "./components/Menu";
 import Main from "./components/Main"
@@ -5,8 +6,26 @@ import Main from "./components/Main"
 import style from "./App.module.css";
 
 function App() {
-  return (
-    <div className={style.app}>
+  // return (
+  //   <div className={style.app}>
+  //     <Header />
+
+  //     <div className={style.container}>
+  //       <Menu />
+
+  //        <Main />
+  //     </div>
+  //   </div>
+  // );
+}
+
+class App extends Component {
+
+  
+
+  render() {
+    return (
+      <div className={style.app}>
       <Header />
 
       <div className={style.container}>
@@ -15,7 +34,7 @@ function App() {
          <Main />
       </div>
     </div>
-  );
+    )
+  }
 }
-
 export default App;
--- client/src/components/Header/NavigationItem/index.js
+++ client/src/components/Header/NavigationItem/index.js
@@ -1,1 +1,2 @@
 export {default} from './NavigationItem';
+//vmesto da pi6em export i inport stava na edin red
