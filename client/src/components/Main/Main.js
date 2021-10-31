a1ea71b197fba066f27fb81153b1511a56ceffcf
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-10-31 12:41:21 +0200

    gg
    gg

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 //<fragment> ako polzvame vmesto
 // 6te ima element koito da darji h1 i p vmesto main i nqma da go ima v html
-
+// ili prazen tag <>==<Fragment>
 import "./Main.css";
 
 const Main = () => {
