ea53667f2ca751a680a1bcfb59d7ac306266bdd9
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-11-01 11:24:37 +0200

    gg
    gg

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -2,7 +2,9 @@ import { Fragment } from "react";
 //<fragment> ako polzvame vmesto
 // 6te ima element koito da darji h1 i p vmesto main i nqma da go ima v html
 // ili prazen tag <>==<Fragment>
+import Post from '../Post'
 import "./Main.css";
+
 
 const Main = ({
   posts
@@ -11,10 +13,16 @@ const Main = ({
   return (
     <main className='main-container'>
        <h1>Soooooooome Heading</h1>
-
-      {posts.map(x => 
-           <p key={x._id}>{x.content}</p>
-      )}
+      
+      <div className="posts">
+         {posts.map(x => 
+              <Post 
+             key={x._id}
+             content={x.content}
+              author={x.author}
+            />
+           )}
+      </div>
     </main>
   );
 };
