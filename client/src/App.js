6cce4c5d8e5e30be0ddc2f4603ecb05b0f9cda25
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-11-01 09:13:49 +0200

    withClass
    gg

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,40 @@
 import { Component } from "react";
+
+import postService from './services/postService';
+
 import Header from "./components/Header/Header";
 import Menu from "./components/Menu";
 import Main from "./components/Main"
-
 import style from "./App.module.css";
 
-function App() {
-  // return (
-  //   <div className={style.app}>
-  //     <Header />
+// function App() {
+//   return (
+//     <div className={style.app}>
+//       <Header />
 
-  //     <div className={style.container}>
-  //       <Menu />
+//       <div className={style.container}>
+//         <Menu />
 
-  //        <Main />
-  //     </div>
-  //   </div>
-  // );
-  class App extends Component {
-  
-    componentDidMount() {
-      
-    }
-  
-    render() {
-      return (
-        <div className={style.app}>
-        <Header />
-  
-        <div className={style.container}>
-          <Menu />
-  
-           <Main />
-        </div>
+//          <Main />
+//       </div>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+ render() {
+  return (
+    <div className={style.app}>
+      <Header />
+
+      <div className={style.container}>
+        <Menu />
+
+         <Main />
       </div>
-      )
-    }
-  }
+    </div>
+  );
+ }
 }
 
 export default App;
