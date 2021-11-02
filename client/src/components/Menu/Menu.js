d0fe9cc297d16e75713c62bd912519c589dc4bd9
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-11-02 11:44:55 +0200

    gg
    gg

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,21 +1,22 @@
+import { useState } from 'react';
+//da znae koe e natisnato
 import MenuItem from './MenuItem'
 
 import "./Menu.css";
+import { MENU_ITEMS } from './MenuConstants'
+
+
 
 const  Menu =() =>  {
+     const [currentItem, setCurrentItem] = useState();
+
+
   return (
    <aside className="menu">
-    <MenuItem>Going to 1</MenuItem>
-    <MenuItem>Going to 2</MenuItem>
-    <MenuItem>Going to 3</MenuItem>
-    <MenuItem>Going to 4</MenuItem>
-    <MenuItem>Going to 5</MenuItem>
-    <MenuItem>Going to 6</MenuItem>
-    <MenuItem>Going to 7</MenuItem>
-    <MenuItem>Going to 8</MenuItem>
-    <MenuItem>Going to 9</MenuItem>
-    <MenuItem>Going to 10</MenuItem>
-    <MenuItem>Going to 11</MenuItem>
+    {MENU_ITEMS.map(x => 
+        <MenuItem key={x.id} id={x.id} onClick={setCurrentItem}>{x.text}</MenuItem>
+
+        )}
    </aside>
   );
 }
---
+++ client/src/components/Menu/MenuConstants.js
@@ -0,0 +1,14 @@
+
+
+export const MENU_ITEMS = [
+    {id: 1, text: 'Going to'},
+    {id: 2, text: 'Going to'},
+    {id: 3, text: 'Going to'},
+    {id: 4, text: 'Going to'},
+    {id: 5, text: 'Going to'},
+    {id: 6, text: 'Going to'},
+    {id: 7, text: 'Going to'},
+    {id: 8, text: 'Going to'},
+    {id: 9, text: 'Going to'},
+    {id: 10, text: 'Going to'}
+];
