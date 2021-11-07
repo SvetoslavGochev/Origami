0f42a3a8f4ab12aa44f4c83328e6ca7fcda03222
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-11-07 10:51:22 +0200

    goodMornig
    gg

---
+++ client/src/components/Footer/footer.js
@@ -0,0 +1,14 @@
+const footer = ({
+    posts
+  })=> {
+    console.log(posts)
+    return (
+      <main className='main-container'>
+         <h1>Soooooooome Heading</h1>
+        
+      
+      </main>
+    );
+  };
+  
+  export default footer;
