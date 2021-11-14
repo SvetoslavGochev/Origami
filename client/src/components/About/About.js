66dd9472d976d07861202ef415434bdd2776ed1f
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-11-14 11:19:05 +0200

    gg
    gg

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,4 +1,4 @@
-import { Redirect, useHistory }  from 'react-router-dom';
+import { Redirect, useHistory, Switch, Route }  from 'react-router-dom';
  
 
 const About = ({
@@ -7,17 +7,26 @@ const About = ({
     history
 }
 ) => {
-if(Math.random() > 0.5 ) {
+//     const histori = useHistory();
+// if(Math.random() > 0.5 ) {
 
-    return <Redirect path="/"/>
-}
+//     // return <Redirect path="/"/>
+//     histori.push("/");
+//     return null;
+// }
 
-const histori = useHistory();
     
     return (
         <main className="main-container">
-    <h1>About {match.params.name} Page</h1>
-    </main>
+            <h1>About {match.params.name} Page</h1>
+            {/* da pi6e pe6p 5 7 8 da izpisva tam kadeto sme natisnali */}
+            <Switch>
+                <Route path="/about/pesho">
+                <h2>Pe6o is the best</h2>
+                </Route>
+            </Switch>
+            {/*  kogatoi se natisne pe6o 6te pokage dopalnmitelno i tova */}
+        </main>
     ) ;
 }
     
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import style from "./Header.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom';
+
 const Header = () => {
   return (
     <nav className={style.navigation}>
@@ -9,19 +10,19 @@ const Header = () => {
           <img src="white-origami-bird.png" alt="white-origami"></img>
         </li>  
         
-       <Link to="/"><NavigationItem>Home</NavigationItem></Link>
-       <Link to="/about"><NavigationItem>About</NavigationItem></Link>
-       <Link to="contact"><NavigationItem>ContactUs</NavigationItem></Link>
-       <Link to="/about/pesho"><NavigationItem>Pesho</NavigationItem></Link>
-       <Link to="/about/gosho"><NavigationItem>Going to 4</NavigationItem></Link>
-       <Link to="/about/pe6o2"><NavigationItem>Going to 5</NavigationItem></Link>
-       <Link to="/about/pe6o3"><NavigationItem>Going to 6</NavigationItem></Link>
-       <Link to="/about/pe6o4"><NavigationItem>Going to 7</NavigationItem></Link>
-       <Link to="/about/pe6o5"><NavigationItem>Going to 8</NavigationItem></Link>
-       <Link to="/about/pe6o6"><NavigationItem>Going to 9</NavigationItem></Link>
-       <Link to="/about/pe6o7"><NavigationItem>Going to 10</NavigationItem></Link>
-       <Link to="/about/pe6o8"><NavigationItem>Going to 11</NavigationItem></Link>
-
+       <NavLink activeClassName="nav-item" exact={true} to="/"><NavigationItem>Home</NavigationItem></NavLink>
+       <NavLink activeClassName="nav-item" exact={true} to="/about"><NavigationItem>About</NavigationItem></NavLink>
+       <NavLink activeClassName="nav-item" exact={true} to="contact"><NavigationItem>ContactUs</NavigationItem></NavLink>
+       <NavLink activeClassName="nav-item" exact={true} to="/about/pesho"><NavigationItem>Pesho</NavigationItem></NavLink>
+       <NavLink activeClassName="nav-item" exact={true} to="/about/gosho"><NavigationItem>Going to 4</NavigationItem></NavLink>
+       <NavLink activeClassName="nav-item" exact={true} to="/about/pe6o2"><NavigationItem>Going to 5</NavigationItem></NavLink>
+       <NavLink activeClassName="nav-item" exact={true} to="/about/pe6o3"><NavigationItem>Going to 6</NavigationItem></NavLink>
+       <NavLink activeClassName="nav-item" exact={true} to="/about/pe6o4"><NavigationItem>Going to 7</NavigationItem></NavLink>
+       <NavLink activeClassName="nav-item" exact={true} to="/about/pe6o5"><NavigationItem>Going to 8</NavigationItem></NavLink>
+       <NavLink activeClassName="nav-item" exact={true} to="/about/pe6o6"><NavigationItem>Going to 9</NavigationItem></NavLink>
+       <NavLink activeClassName="nav-item" exact={true} to="/about/pe6o7"><NavigationItem>Going to 10</NavigationItem></NavLink>
+       <NavLink activeClassName="nav-item" exact={true} to="/about/pe6o8"><NavigationItem>Going to 11</NavigationItem></NavLink>
+{/* poslednoto natisnato e 4erveno ako e bez exact={true} vsi4ki natisnati sa 4erveni */}
       
       </ul>
     </nav>
